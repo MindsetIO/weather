a3a71f6a92d65c479c16688d77eb5b892ed7c882
mindset.io team <team@mindset.io>
2021-03-27 23:45:26 -0700

    Update weather_app.py

--- weather_app.py
+++ weather_app.py
@@ -6,6 +6,7 @@ from weather import Weather
 
 
 def main(zipcode, units="F"):
+    print("timeit")
     wtr = Weather.from_zipcode(zipcode, units=units)
     text = wtr.text_report()
     return {"text": text, **wtr.current.data}
